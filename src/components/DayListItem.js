fddfa45784ffe8a37a70228a56dfaef3ab145799
Billy Wong <bwong.yh@protonmail.com>
2022-07-13 02:55:59 +0900

    add style

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
 function DayListItem(props) {
+  let dayClass = classNames({
+    'day-list__item': true,
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">Day Name</h2>
       <h3 className="text--light">X spots remaining</h3>
     </li>
